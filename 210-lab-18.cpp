2ba0e54f7077b77a70712149c51de75fad9f136b
Hussain <Aahil.Hussain@ejgallo.com>
2024-10-06 18:12:22 -0700

    Second commit

--- 210-lab-18.cpp
+++ 210-lab-18.cpp
@@ -1,3 +1,31 @@
-//hello 
+#include <iostream>
+#include <string>
+using namespace std;
 
-//testing commit changes
+struct Review {
+    float rating;
+    string comments;
+    Review* next;
+};
+
+Review* head = nullptr;
+
+
+//function to add in front
+void addNodeAtHead(float rating, string comments) {
+    Review* newReview = new Review{rating, comments, head};
+    head = newReview;
+}
+
+//function to add to the back
+void addNodeAtTail(float rating, string comments) {
+    if (head == nullptr) {
+        head = new Review{rating, comments, nullptr};
+        return;
+    }
+    Review* temp = head;
+    while (teamp-> next != nullptr) {
+        
+    }
+}
+
