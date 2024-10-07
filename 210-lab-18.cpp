ef140ab9fb86dff18357f45b9c27e9ded78935a2
Hussain <Aahil.Hussain@ejgallo.com>
2024-10-06 18:40:41 -0700

    4th commit

--- 210-lab-18.cpp
+++ 210-lab-18.cpp
@@ -38,6 +38,58 @@ void outputReviews() {
     }
 }
 
+void outputReviewsAndAverage() {
+    Review* temp = head;
+    float sum = 0;
+    int count = 0;
+    while (temp != nullptr) {
+        cout << "Rating: " << temp -> rating << ", Comments: " << temp->comments << endl;
+        sum += temp-> rating;
+        temp = temp->next;
+        count++;
+    }
+    if (count > 0)
+    {
+        cout << "Average: " << sum / (count - 1) << endl;
+    }
+}
+
 int main() {
+    int choice;
+    cout << "Which linked list method should we use?\n";
+    cout << "   [1] New nodes are added at the head of the linked list\n";
+    cout << "   [2] New nodes are added at the tail of the linked list\n";
+    cout << "   Choise: ";
+    cin >> choice;
+
+    if(choice != 1 && choice != 2) {
+        cout << "Invalid choice. Exiting...\n";
+        return 1; 
+    }
+
+    char anotherReview ='y';
+    while (anotherReview == 'y') {
+        float rating;
+        string comments;
+        cout << "Enter review rating 0-5: ";
+        cin >> rating;
+        cout << "Enter review comments: ";
+        cin.ignore();
+        getline(cin, comments);
+
+        if (choice == 1) {
+            addNodeAtHead(rating, comments);
+        } else if (choice == 2) {
+            addNodeAtTail(rating, comments);
+        }
+
+        cout << "Enter another review? Y/N: ";
+        cin >> anotherReview;
+    }
+
+    outputReviews();
+
     return 0;
+
+
 }
