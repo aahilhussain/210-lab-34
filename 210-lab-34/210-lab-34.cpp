32215e83157c7fc6f6a34cc561b80f7bcb4e8702
Hussain <Aahil.Hussain@ejgallo.com>
2024-11-21 14:29:19 -0800

    2nd commit

--- 210-lab-34/210-lab-34.cpp
+++ 210-lab-34/210-lab-34.cpp
@@ -1,36 +1,35 @@
 #include <iostream>
 #include <vector>
+#include <unordered_set>
+#include <stack>
+#include <queue>
 
 using namespace std;
 
-const int SIZE = 7;
+const int SIZE = 9;
 
-struct Edge
-{
+struct Edge {
     int src, dest, weight;
 };
+
 typedef pair<int, int> Pair; // Creates alias 'Pair' for the pair<int,int> data type
 
-class Graph
-{
+class Graph {
 public:
     // a vector of vectors of Pairs to represent an adjacency list
     vector<vector<Pair>> adjList;
+
     // Graph Constructor
     Graph(vector<Edge> const &edges)
     {
-        // resize the vector to hold SIZE elements of type vector<Edge>
-        adjList.resize(SIZE);
-        // add edges to the directed graph
+        adjList.resize(SIZE); // resize the vector to hold SIZE elements of type vector<Edge>
         for (auto &edge : edges)
         {
             int src = edge.src;
             int dest = edge.dest;
             int weight = edge.weight;
-            // insert at the end
-            adjList[src].push_back(make_pair(dest, weight));
-            // for an undirected graph, add an edge from dest to src also
-            adjList[dest].push_back(make_pair(src, weight));
+            adjList[src].push_back(make_pair(dest, weight));  // insert at the end
+            adjList[dest].push_back(make_pair(src, weight)); // for an undirected graph, add an edge from dest to src also
         }
     }
     // Print the graph's adjacency list
@@ -45,7 +44,59 @@ public:
             cout << endl;
         }
     }
+
+    //DFS Depth First Search
+    void DFS(int start) {
+        unordered_set<int> visited;
+        stack<int> stack;
+        stack.push(start);
+
+        cout << "DFS starting from vertex " << start << ": ";
+        while (!stack.empty()) {
+            int v = stack.top();
+            stack.pop();
+
+            if (visited.find(v) == visited.end()) {
+                cout << v << " ";
+                visited.insert(v);
+            }
+
+            for (auto &neighbor : adjList[v]) {
+                if (visited.find(neighbor.first) == visited.end()) {
+                    stack.push(neighbor.first);
+                }
+            }
+        }
+
+        cout << endl;
+    }
+
+    void BFS(int start) {
+    unordered_set<int> visited;
+    queue<int> queue;
+    queue.push(start);
+
+        cout << "BFS starting from vertex " << start << ": ";
+        while(!queue.empty()) {
+            int v = queue.front();
+            queue.pop();
+
+            if (visited.find(v) == visited.end()) {
+                cout << v << " ";
+                visited.insert(v);
+            }
+
+            for (auto &neighbor : adjList[v]) {
+                if (visited.find(neighbor.first) == visited.end()) {
+                    queue.push(neighbor.first);
+                }
+            }
+        }
+
+    cout << endl;
+    }
 };
+
 int main()
 {
     // Creates a vector of graph edges/weights
@@ -64,5 +115,9 @@ int main()
     Graph graph(edges);
     // Prints adjacency list representation of graph
     graph.printGraph();
+
+    //Perform DFS and BFS
+    graph.DFS(0);
+    graph.BFS(0);
     return 0;
 }
